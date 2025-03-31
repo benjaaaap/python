444b76b7a17195a4538559b04c4f708d7196f014
benjaaaap <benjamin.poblete.donoso@alumnos.sip.cl>
2025-03-31 12:00:14 -0300

    agregando elif notas

---
+++ Control de flujos/nota.py
@@ -0,0 +1,15 @@
+#Benjamin Poblete
+#31-03-2025
+
+nota = 60
+
+if nota >= 70:
+    print("paso yujuu!!!")
+elif nota >60:
+    print ("paso")
+elif nota >50:
+    print("vas por buen camino")
+elif nota >=40:
+    print("nota es nota igual es azul")
+else:
+    print("no te rindas, cada error es una oportunidad")
