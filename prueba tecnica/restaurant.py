4cd08de4ac8d72a1f59013ffae8ac7d6cb3a7ee4
benjaaaap <benjamin.poblete.donoso@alumnos.sip.cl>
2025-04-09 08:34:34 -0400

    creando prueba 1

---
+++ prueba tecnica/restaurant.py
@@ -0,0 +1,16 @@
+#Benjamin Poblete
+#09-04-2025
+
+rating = 7.0
+
+# Sistema de calificación
+if rating > 4.5:
+    print("Extraordinario")
+elif rating > 4.0:
+    print("Excelente")
+elif rating > 3.0:
+    print("Bueno")
+elif rating > 2.0:
+    print("Regular")
+else:
+    print("Pesimo")
